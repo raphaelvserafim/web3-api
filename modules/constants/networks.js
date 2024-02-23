8efc31757091a9a891b304390b345a63ef4e6a29
Raphael Serafim <raphaelvserafim@gmail.com>
2024-02-23 17:53:20 -0500

    feat: new networks

--- modules/constants/networks.js
+++ modules/constants/networks.js
@@ -1,5 +1,12 @@
 const NETWORKS = {
-  BLAST: { MAIN: "", TEST: "https://sepolia.blast.io" }
+  BITCOIN: { NAME: "Bitcoin", MAIN: "https://blockchain.info", TEST: "https://testnet.blockchain.info" },
+  ETHEREUM: { NAME: "Ethereum", MAIN: "https://mainnet.infura.io/v3", TEST: "https://rinkeby.infura.io/v3" },
+  BSC: { NAME: "Binance Smart Chain", MAIN: "https://bscscan.com", TEST: "https://testnet.bscscan.com" },
+  POLYGON: { NAME: "Polygon", MAIN: "https://polygonscan.com", TEST: "https://mumbai.polygonscan.com" },
+  SOLANA: { NAME: "Solana", MAIN: "https://explorer.solana.com", TEST: "https://testnet.solana.com" },
+  CARDANO: { NAME: "Cardano", MAIN: "https://cardanoscan.io", TEST: "https://testnet.cardanoscan.io" },
+  BLAST: { NAME: "Blast", MAIN: "", TEST: "https://sepolia.blast.io" },
+
 };
 
-module.exports = NETWORKS
+module.exports = NETWORKS;
